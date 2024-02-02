7d0b4395cccfadfe50c2bc35e0c21e363447b237
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-02-01 22:17:55 -0300

    hoje

--- Exercicios/ex6.js
+++ Exercicios/ex6.js
@@ -1,5 +1,7 @@
 const ano = 1004;
 
-for (var i = ano; i <= 2017; i = i + 4) {
-    console.log(i)
+for (var i = ano; i <= 2017; i += 4) {
+    if (!(i % 100 === 0) || i % 4 === 0) {
+        console.log(i)
+    }
 }
