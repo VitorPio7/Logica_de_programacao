8d8a47a5ebd691856b55a77d28cc85dd67be9a91
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-02-21 15:17:42 -0300

    ola
    ola

--- Exercicios/ex20.js
+++ Exercicios/ex20.js
@@ -1,16 +1,5 @@
-function points(games) {
-    var count = 0;
-    for (score of games) {
-        let res = score.split(':')
-        if (res[0] > res[1]) {
-            count += 3
-        } else if (res[0] == res[1]) {
-            count += 1
-        }
-    }
-    return count
+function solution(number) {
+
 }
 
-const jogos = ["3:1", "2:2", "0:1"]
-
-console.log(points(jogos))
+console.log(solution(10))
